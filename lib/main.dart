900008d71bfb67e81ca285c1a76bac4686d21bcb
Muhammadkhan <muhammadflutter07@gmail.com>
2026-02-18 17:07:32 +0500

    Initial commit without Ui-demo

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// import 'package:appbar/practice.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(debugShowCheckedModeBanner: false, home: PracticeScreen()),
+//   );
+// }
+void main() {
+  int i = 1;
+
+  while (i <= 20) {
+    print(i);
+    i++;
+  }
+}
