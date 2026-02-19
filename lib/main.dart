730f321a7516b35cc27e2a41802ffbaaa6d59b7f
Muhammadkhan <muhammadflutter07@gmail.com>
2026-02-19 14:37:59 +0500

    Add new statement

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,25 @@
 //     MaterialApp(debugShowCheckedModeBanner: false, home: PracticeScreen()),
 //   );
 // }
-void main() {
-  int i = 1;
+import 'dart:io';
 
-  while (i <= 20) {
-    print(i);
-    i++;
+void main() {
+  String correctpassword = "1234";
+  String userinput = "";
+  int attempts = 0;
+  while (userinput != correctpassword && attempts < 3) {
+    print('Enter password');
+    userinput = stdin.readLineSync()!;
+
+    if (userinput != correctpassword) {
+      attempts++;
+      print('Wrong password');
+    }
   }
+  if (userinput == correctpassword) {
+    print('successfull');
+  } else {
+    print('Account locked');
+  }
+  ;
 }
