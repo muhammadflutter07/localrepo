8ee38ff661b023648f7c28e22fbc7b65a7793f36
Muhammadkhan <muhammadflutter07@gmail.com>
2026-02-24 15:23:07 +0500

    New statement of if else

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 void main() {
-  int a = 8;
-  int b = 9;
-
-  print(a * b);
+  int day = 7;
+  if (day == 1) {
+    print('Monday');
+  } else if (day == 2) {
+    print('Tuesday');
+  } else if (day == 3) {
+    print('Wednesday');
+  } else if (day == 4) {
+    print('thurs');
+  } else if (day == 5) {
+    print('FRI');
+  } else {
+    print('Invalid day');
+  }
 }
