db2ad124f44325f96f03604e7ae7a0e0b37b4a15
Muhammadkhan <muhammadflutter07@gmail.com>
2026-02-26 13:57:55 +0500

    Add new statement about type casting

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 void main() {
-  int day = 7;
-  if (day == 1) {
-    print('Monday');
-  } else if (day == 2) {
-    print('Tuesday');
-  } else if (day == 3) {
-    print('Wednesday');
-  } else if (day == 4) {
-    print('thurs');
-  } else if (day == 5) {
-    print('FRI');
-  } else {
-    print('Invalid day');
-  }
+  var a = 100;
+  var b = 200;
+
+  print(a is int);
+  print(b is! int);
 }
