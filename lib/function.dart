aa50f291c6bd1c315995873425480d2a69da0310
Muhammadkhan <muhammadflutter07@gmail.com>
2026-02-27 12:08:49 +0500

    functions in Dart

---
+++ lib/function.dart
@@ -0,0 +1,10 @@
+int calculatearea(int lenght, int widht) {
+  return lenght * widht;
+}
+
+void main() {
+  int area = calculatearea(10, 04);
+  int doublearea = area * 2;
+  print(area);
+  print(doublearea);
+}
--- lib/type.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  dynamic x = 100;
-  int y = x as int;
-  print(y * 2);
-}
