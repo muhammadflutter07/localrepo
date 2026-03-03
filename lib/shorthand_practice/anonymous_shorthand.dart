a723084ca2eb97ddd7b37beee78710a8749c2bab
Muhammadkhan <muhammadflutter07@gmail.com>
2026-03-03 12:48:12 +0500

    Shorthand and anonymous function

---
+++ lib/shorthand_practice/anonymous_shorthand.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  greet("muhammad");
+  print(square(6));
+  print(greetuser("Khan"));
+  print(greetuser("Khalid", "Dr"));
+}
+
+void greet(String name) {
+  print(name);
+}
+
+int square(int n) => n * n;
+
+String greetuser(String name, [String title = "Mr"]) => "Hello $title $name!";
