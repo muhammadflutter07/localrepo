262497e565a52ea9d846e134f82035d659c5ff70
Muhammadkhan <muhammadflutter07@gmail.com>
2026-03-05 11:48:10 +0500

    positional optional parameters

--- lib/parameters/optional_para.dart
+++ lib/parameters/optional_para.dart
@@ -2,8 +2,9 @@ void main() {
   greetuser("faiz", 13);
   greetuser("faiz");
   greetuser();
+  greetuser();
 }
 
 void greetuser([String? name = "Guest", int? no]) {
-  print("Hello $name, your room No $no");
+  print("Hello $name, youre room No $no");
 }
