02bb0e53c87c0ea4df3d70c5ffa5deae1e96d855
Muhammadkhan <muhammadflutter07@gmail.com>
2026-03-26 10:57:48 +0500

    exception handling

---
+++ lib/Exception_Handling/exception.dart
@@ -0,0 +1,13 @@
+void main() {
+  try {
+    int a = 10;
+    int b = 0;
+    if (b == 0) {
+      print('cannot divide by zero');
+    }
+    int x = a ~/ b;
+    print(x);
+  } catch (e) {
+    print('exception:$e');
+  }
+}
