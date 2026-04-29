03e8a6ff1122c66091e23ab2e7bbda2fa8b79c0e
Muhammadkhan <muhammadflutter07@gmail.com>
2026-04-29 17:16:40 +0500

    basic encasulation

---
+++ lib/OOP/encapsulation/encapsulation.dart
@@ -0,0 +1,17 @@
+class Marks {
+  int _marks = 0;
+
+  int get marks => _marks;
+
+  void setmarks(int value) {
+    if (value >= 0 && value <= 100) {
+      _marks = value;
+    } else {
+      print('invalid marks');
+    }
+  }
+}
+
+void main() {
+  Marks marks = Marks();
+}
