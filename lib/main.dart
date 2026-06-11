6387225f4b75f7a4feeafa4f7d8a2050d2691ae1
Muhammadkhan <muhammadflutter07@gmail.com>
2026-06-11 14:42:32 +0500

    Widgets practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:appbar/widgets/common_widgets.dart';
+import 'package:appbar/widgets/otp_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppScreen());
   }
 }
---
+++ lib/widgets/otp_screen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class MyAppScreen extends StatelessWidget {
+  const MyAppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyScreen());
+  }
+}
+
+class MyScreen extends StatefulWidget {
+  const MyScreen({super.key});
+
+  @override
+  State<MyScreen> createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<MyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_ios),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Verification",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(width: 48),
+                ],
+              ),
+
+              SizedBox(height: 40),
+
+              Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xFF2C2C2E),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(Icons.message, color: Colors.white, size: 30),
+              ),
+              SizedBox(height: 24),
+
+              Text(
+                "Verify your Account",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                "Enter the 6 digit code that we sent to\n+92 300-0000000",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black87, fontSize: 14),
+              ),
+              SizedBox(height: 20),
+
+              Pinput(
+                length: 6,
+
+                defaultPinTheme: PinTheme(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black),
+                  ),
+
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                preFilledWidget: Text(
+                  '—',
+                  style: TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              Text(
+                "02:30s",
+                style: TextStyle(color: Colors.black54, fontSize: 14),
+              ),
+              SizedBox(height: 32),
+
+              SizedBox(
+                width: double.infinity,
+                height: 52,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    "Verify",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              RichText(
+                text: TextSpan(
+                  text: "Didn't receive the code",
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                  children: [
+                    WidgetSpan(child: SizedBox(width: 5)),
+                    TextSpan(
+                      text: "Resend",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
