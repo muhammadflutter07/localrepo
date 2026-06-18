ab2990b34c4503b76060e4b949ee70e177b6c213
Muhammadkhan <muhammadflutter07@gmail.com>
2026-06-18 15:32:29 +0500

    splash screen  practice for Resume project

---
+++ lib/widgets/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MyAppScreen extends StatefulWidget {
+  const MyAppScreen({super.key});
+
+  @override
+  State<MyAppScreen> createState() => _MyAppScreenState();
+}
+
+class _MyAppScreenState extends State<MyAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2D2D),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(Icons.description, size: 40),
+              ),
+
+              SizedBox(height: 15),
+              Text(
+                "Ace Resume",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
+
+              Text(
+                "Resume that helps you ace your job\n hunt",
+                style: TextStyle(color: Colors.white54),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
